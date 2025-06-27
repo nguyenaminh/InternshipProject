c05ed108ce14a99b5e2fdac7d0b07e056ce15405
Minh <naminh@IfiSolution.local>
2025-06-27 11:07:03 +0700

    Override old repo

--- iot-producer/src/test/java/com/example/iot_producer/IotProducerApplicationTests.java
+++ iot-producer/src/test/java/com/example/iot_producer/IotProducerApplicationTests.java
@@ -1,26 +1,11 @@
 package com.example.iot_producer;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
+
 
 @SpringBootTest
-@TestPropertySource("classpath:application-test.properties")
 class IotProducerApplicationTests {
-
-    @TestConfiguration
-    static class MockRabbitConfig {
-        @Bean(name = "mockRabbitTemplate")
-        public RabbitTemplate rabbitTemplate() {
-            return Mockito.mock(RabbitTemplate.class);
-        }
-    }
 
     @Test
     void contextLoads() {
